0451648a68354b7e7440ba53f6c52b47e91e918c
Jason Doo <jeong7496@gmail.com>
2018-11-11 12:34:28 -0800

    switch to postgresql

--- app.js
+++ app.js
@@ -1,119 +1,80 @@
 const express = require('express');
-const mysql = require('mysql');
 const app = express();
 
-// MySQL connection
-const db = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'fitnessdb'
+const pg = require('pg');
+const client = new pg.Client({
+    user: "ldkazcmbjgsuwd",
+    password: "5d89b5fd97cdf96343d3a97e34e3802890922b99e9198ae45b93fafdbaf3badb",
+    database: "dd7dpk6dg5oidc",
+    port: 5432,
+    host: "ec2-50-19-127-158.compute-1.amazonaws.com",
+    ssl: true
 });
+client.connect();
 
-// Connect to db
-db.connect((err) => {
-    if (err) {
-        console.log('Could not connect to database');
-        return;
-    }
-    console.log('Successfully connected!');
-});
-
-// Route for creating a database
-app.get('/createdb', (req, res) => {
-   let query = 'CREATE DATABASE fitnessdb';
-   db.query(query, (err, result) => {
-       if (err) {
-           console.log(err.sqlMessage);
-           res.send(err.sqlMessage);
-           return;
-       }
-       console.log(result);
-       res.send('Database created!');
-   });
-});
-
-// Creates BRANCH table and populates it with data
-app.get('/createbranch', (req, res) => {
-    let branch = 'CREATE TABLE Branch(\n' +
-        'branch_id INTEGER,\n' +
-        'address CHAR(50),\n' +
-        'phone_no CHAR(10),\n' +
-        'PRIMARY KEY(branch_id))\n';
-    db.query(branch, (err, result) => {
+app.get('/', async function (req, res) {
+    let query = 'CREATE TABLE Branch(branch_id INTEGER, address CHAR(50),\n' +
+        'phone_no CHAR(10), PRIMARY KEY(branch_id))';
+    client.query(query, (err, res) => {
         if (err) {
-            console.log(err.sqlMessage);
-            res.send(err.sqlMessage);
-            return;
+            console.log(err.stack)
+        } else {
+            console.log(res.rows[0])
         }
-        console.log('Created Branch Table');
     });
-
-    let insertBranch = 'INSERT INTO Branch SET ?';
-    // Mock data
-    let branches = [
-        {
-            branch_id: 101,
-            address: '420 Bubble Tea Street',
-            phone_no: '7783121224'
-        },
-        {
-            branch_id: 123,
-            address: '6789 Muscular Drive',
-            phone_no: '7789881234'
-        },
-        {
-            branch_id: 103,
-            address: '202-5122 North Avenue',
-            phone_no: '7789801004'
-        },
-        {
-            branch_id: 102,
-            address: '9088 Protein Street',
-            phone_no: '7789086565'
-        },
-    ];
-
-    branches.forEach((branch) => {
-        db.query(insertBranch, branch, (err, result) => {
-            if (err) {
-                console.log(err.sqlMessage);
-                res.send(err.sqlMessage);
-                return;
-            }
-            console.log('Branch ' + branch.branch_id + ' created');
-        });
-    });
-});
-
-app.get('/getbranches', (req, res) => {
-    let query = 'SELECT * FROM Branch';
-    db.query(query, (err, results) => {
-        if (err) {
-            console.log(err.sqlMessage);
-            res.send(err.sqlMessage);
-            return;
-        }
-        res.send(results);
-    });
-});
-
-app.get('/getbranch/:id', (req, res) => {
-   let query = `SELECT * FROM Branch WHERE branch_id = ${req.params.id}`;
-    db.query(query, (err, result) => {
-        if (err) {
-            console.log(err.sqlMessage);
-            res.send(err.sqlMessage);
-            return;
-        }
-        res.send(result);
-    });
-});
-
-app.get('/', function (req, res) {
     res.sendFile(__dirname + '/public/index.html');
 });
 
+app.get('/populatebranch', (req, res) => {
+    let insertBranch = 'INSERT INTO Branch(branch_id, address, phone_no) VALUES ($1, $2, $3)';
+    let branches = [
+        [
+            '101',
+            '420 Bubble Tea Street',
+            '7783121224'
+        ],
+        [
+            '123',
+            '6789 Muscular Drive',
+            '7789881234'
+        ],
+        [
+            '103',
+            '202-5122 North Avenue',
+            '7789801004'
+        ],
+        [
+            '102',
+            '9088 Protein Street',
+            '7789086565'
+        ]
+    ];
+
+    branches.forEach((branch) => {
+        client.query(insertBranch, branch, (err, result) => {
+            if (err) {
+                console.log(err.message);
+            } else {
+                console.log(result.rows[0]);
+            }
+        });
+    });
+    res.send('done');
+});
+
+app.get('/listbranches', async function (req, res) {
+    let query = 'SELECT * FROM Branch';
+    client.query(query, (err, result) => {
+        if (err) {
+            res.send(err.message);
+            return;
+        } else {
+            res.send(JSON.stringify(result.rows));
+            return;
+        }
+    });
+});
+
 app.listen(process.env.PORT || 5000, () => {
-    console.log('Server started');
+    console.log('Server started succesfully.');
 });
