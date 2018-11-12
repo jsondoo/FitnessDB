75f3a2fe01a3c5531be95b5a8ffb972d7662cebc
Lim <a.hui11@alumni.ubc.ca>
2018-11-12 15:58:22 -0800

    added stuff

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ const express = require('express');
 const app = express();
 
 let branchData = require("./data/branch_data");
+let machineData = require("./data/branch_machines");
+let roomData = require("./data/room");
+let personalTrainerData = require("./data/branch_personalTrainer");
 
 const pg = require('pg');
 const client = new pg.Client({
@@ -17,6 +20,7 @@ client.connect();
 app.get('/', async function (req, res) {
     let query = 'CREATE TABLE IF NOT EXISTS Branch(branch_id INTEGER, address CHAR(50),\n' +
        'phone_no CHAR(12), PRIMARY KEY(branch_id))';
+	   
     client.query(query, (err, result) => {
         if (err) {
             console.log(err.stack)
@@ -42,6 +46,61 @@ app.get('/populatebranch', (req, res) => {
     res.send('done');
 });
 
+app.get('/populatemachine', (req, res) => {
+	let createMachineQuery = 'CREATE TABLE Machine (mid INTEGER, date_bought DATE, condition CHAR(5), \n'+
+	'last_maintenance DATE, cost INTEGER, type CHAR(20), branch_id INTEGER, PRIMARY KEY (mid), \n'+
+	'FOREIGN KEY (branch_id) REFERENCES Branch ON DELETE SET NULL ON UPDATE CASCADE)';
+	client.query(createMachineQuery, (err, result) => {
+		if (err) {
+			console.log(err.message);
+		}
+    });
+    let insertMachine = 'INSERT INTO Machine(mid, date_bought, condition, last_maintenance, cost, type, branch_id) VALUES ($1, $2, $3, $4, $5, $6, $7)';
+    machineData.forEach((machine) => {
+		client.query(insertMachine, machine, (err, result) => {
+            if (err) {
+                console.log(err.message);
+            } else {
+                console.log(result.rows[0]);
+            }
+        });
+    });
+    res.send('done');
+});
+
+app.get('/populateroom', (req, res) => {
+	let createRoomQuery = 'CREATE TABLE IF NOT EXISTS Room(room_num INTEGER, capacity INTEGER,PRIMARY KEY (room_num))';
+	client.query(createRoomQuery);
+    let insertRoom = 'INSERT INTO Room(room_num, capacity) VALUES ($1, $2)';
+    roomData.forEach((room) => {
+        let arr = [room.room_num, room.capacity];
+        client.query(insertRoom, arr, (err, result) => {
+            if (err) {
+                console.log(err.message);
+            } else {
+                console.log(result.rows[0]);
+            }
+        });
+    });
+    res.send('done');
+});
+
+app.get('/populatepersonaltrainer', (req, res) => {
+	let createPersonalTrainerQuery = 'CREATE TABLE PersonalTrainer(certification CHAR(5),sid INTEGER, PRIMARY KEY (sid),FOREIGN KEY (sid) REFERENCES Instructor ON DELETE CASCADE ON UPDATE CASCADE)';
+	client.query(createPersonalTrainerQuery);
+    let insertPersoanlTrainer = 'INSERT INTO PersonalTrainer(sid, certification) VALUES ($1, $2)';
+    personalTrainerData.forEach((personTrainer) => {
+        client.query(insertPersoanlTrainer, personTrainer, (err, result) => {
+            if (err) {
+                console.log(err.message);
+            } else {
+                console.log(result.rows[0]);
+            }
+        });
+    });
+    res.send('done');
+});
+
 app.get('/branches', async function (req, res) {
     let query = 'SELECT * FROM Branch';
     client.query(query, (err, result) => {
@@ -54,6 +113,45 @@ app.get('/branches', async function (req, res) {
         }
     });
 });
+
+app.get('/Machine', async function (req, res) {
+    let query = 'SELECT * FROM Machine';
+    client.query(query, (err, result) => {
+        if (err) {
+            res.send(err.message);
+            return;
+        } else {
+            res.send(JSON.stringify(result.rows));
+            return;
+        }
+    });
+});
+app.get('/Room', async function (req, res) {
+    let query = 'SELECT * FROM Room';
+    client.query(query, (err, result) => {
+        if (err) {
+            res.send(err.message);
+            return;
+        } else {
+            res.send(JSON.stringify(result.rows));
+            return;
+        }
+    });
+});
+
+app.get('/PersonalTrainer', async function (req, res) {
+    let query = 'SELECT * FROM PersonalTrainer';
+    client.query(query, (err, result) => {
+        if (err) {
+            res.send(err.message);
+            return;
+        } else {
+            res.send(JSON.stringify(result.rows));
+            return;
+        }
+    });
+});
+
 
 app.listen(process.env.PORT || 5000, () => {
     console.log('Server started succesfully.');
---
+++ data/room.js
@@ -0,0 +1,62 @@
+module.exports = [
+ {
+   "room_num": 196,
+   "capacity": 48
+ },
+ {
+   "room_num": 134,
+   "capacity": 75
+ },
+ {
+   "room_num": 153,
+   "capacity": 45
+ },
+ {
+   "room_num": 198,
+   "capacity": 54
+ },
+ {
+   "room_num": 177,
+   "capacity": 52
+ },
+ {
+   "room_num": 110,
+   "capacity": 43
+ },
+ {
+   "room_num": 132,
+   "capacity": 60
+ },
+ {
+   "room_num": 108,
+   "capacity": 65
+ },
+ {
+   "room_num": 117,
+   "capacity": 36
+ },
+ {
+   "room_num": 145,
+   "capacity": 44
+ },
+ {
+   "room_num": 169,
+   "capacity": 51
+ },
+ {
+   "room_num": 187,
+   "capacity": 80
+ },
+ {
+   "room_num": 165,
+   "capacity": 80
+ },
+ {
+   "room_num": 118,
+   "capacity": 67
+ },
+ {
+   "room_num": 159,
+   "capacity": 39
+ }
+]
