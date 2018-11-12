200eceacacc941b4b6cb126bafe7100361d1e7cb
alexanderluong <alexander.luong@hotmail.com>
2018-11-12 15:12:06 -0800

    Finish function to populate instructor tables

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 
 let branchData = require("./data/branch_data");
+let instructorData = require("./data/instructor_data");
 
 const pg = require('pg');
 const client = new pg.Client({
@@ -14,6 +15,7 @@ const client = new pg.Client({
 });
 client.connect();
 
+// CREATE TABLE FOR BRANCH
 app.get('/', async function (req, res) {
     let query = 'CREATE TABLE IF NOT EXISTS Branch(branch_id INTEGER, address CHAR(50),\n' +
        'phone_no CHAR(12), PRIMARY KEY(branch_id))';
@@ -42,6 +44,47 @@ app.get('/populatebranch', (req, res) => {
     res.send('done');
 });
 
+// CREATE AND POPULATE TABLE FOR INSTRUCTOR
+app.get('/populateinstructor', (req, res) => {
+    // let query = "DROP TABLE INSTRUCTOR";
+    let query = 'CREATE TABLE IF NOT EXISTS Instructor(sid INTEGER, name CHAR(20),\n' +
+        'phone_no CHAR(12), email CHAR(40), date_joined DATE, hourly_wage INTEGER,\n' +
+        'PRIMARY KEY(sid))';
+    client.query(query, (err, result) => {
+        if (err) {
+            console.log(err.message)
+        } else {
+            console.log(result.rows[0])
+        }
+    });
+
+    let insertBranch = 'INSERT INTO Instructor(sid, name, phone_no, email, date_joined, hourly_wage) VALUES ($1, $2, $3, $4, $5, $6)';
+    instructorData.forEach((instructor) => {
+        let arr = [instructor.sid, instructor.name, instructor.phone_num, instructor.email, instructor.date_joined,
+            instructor.hourly_wage];
+        client.query(insertBranch, arr, (err, result) => {
+            if (err) {
+                console.log(err.message);
+            } else {
+                console.log(result[0]);
+            }
+        });
+    });
+    res.send('done');
+});
+
+app.get('/instructors', async function (req, res) {
+    let query = 'SELECT * FROM Instructor';
+    client.query(query, (err, result) => {
+        if (err) {
+            res.send(err.message);
+            return;
+        } else {
+            res.send(JSON.stringify(result.rows));
+            return;
+        }
+    });
+});
 app.get('/branches', async function (req, res) {
     let query = 'SELECT * FROM Branch';
     client.query(query, (err, result) => {
